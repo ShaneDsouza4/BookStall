3ea978ef1233bd79f4d8c2d8af99327db6879515
ShaneDsouza4 <shane.json96@gmail.com>
2024-11-13 13:48:33 -0500

    Login Logout functionality

--- store/views.py
+++ store/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Product
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -9,3 +11,27 @@ def home(request):
 
 def about(request):
         return render(request, 'about.html', {})
+
+def login_user(request):
+    if request.method == 'POST': #Submitting form
+        username = request.POST['username'] #On form name is username
+        password = request.POST['password'] #On form name is password
+
+        #Need to authenticate using django authenticate system
+        user = authenticate(request, username=username, password=password)
+
+        # If form is not empty, log them in
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'You are now logged in')
+            return redirect('home')
+        else:
+            messages.success(request, 'Invalid username or password')
+            return redirect('login')
+    else: #They didn't fill the form, show webpage
+        return render(request, 'login.html')
+
+def logout_user(request):
+    logout(request)
+    messages.success(request, 'You have been logged out.')
+    return redirect('home')
