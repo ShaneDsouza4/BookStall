af1cd5a7e286f5bf95c566c60d8bc98d88fb86c8
ShaneDsouza4 <shane.json96@gmail.com>
2024-11-14 00:42:44 -0500

    Added product to cart and in session

--- cart/cart.py
+++ cart/cart.py
@@ -14,3 +14,14 @@ class Cart():
         # Make sure shopping cart is available on all pages, and keep track
         self.cart = cart
 
+    #Add functionality
+    def add(self, product):
+        product_id = str(product.id)
+
+        #Logic
+        if product_id in self.cart:
+            pass
+        else:
+            self.cart[product_id] = {'price': str(product.price)} #Will be added in session when decoded
+
+        self.session.modified = True
--- cart/views.py
+++ cart/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from http.client import responses
+
+from django.shortcuts import render, get_object_or_404, redirect
+from .cart import Cart #Cart Session
+from store.models import Product
+from django.http import JsonResponse
+
 
 # Create your views here.
 def cart_summary(request):
     return render(request, "cart_summary.html", {})
 
 def cart_add(request):
-    pass
+    # Get the cart
+    cart = Cart(request)
+
+    #Test for Post
+    if request.POST.get('action') == 'post': #Lower case post is mentioned in the Ajax action
+
+        #Get stuff from AJAX
+        product_id = int(request.POST.get('product_id'))
+
+        #Search product in DB
+        product = get_object_or_404(Product, id=product_id)
+
+        #Save to session
+        cart.add(product=product) #Whole model object will be added
+
+        #Return JSON response
+        response = JsonResponse({"Product Name ": product.name}) #Will reference Product Model name
+        return response
 
 def cart_delete(request):
     pass
